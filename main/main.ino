4de298c6be0f8377b850989f565a3e8f1297c47f
Ferchupessoadev <fernandomatiaspessoa471@gmail.com>
2024-08-31 23:06:46 -0300

    changed keypad pins

--- main/main.ino
+++ main/main.ino
@@ -25,8 +25,8 @@ char keys[ROWS][COLS] = {
     {'7', '8', '9', 'C'},
     {'*', '0', '#', 'D'}};
 
-byte rowPins[ROWS] = {9, 8, 7, 6};
-byte colPins[COLS] = {5, 4, 3, 2};
+byte rowPins[ROWS] = {2, 3, 4, 5};
+byte colPins[COLS] = {6, 7, 8, 9};
 
 Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS);
 
